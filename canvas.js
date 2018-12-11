11dd8af200cf4febce23534decc6f33c3c9b89bf
JacobSwetmore <32363293+JacobSwetmore@users.noreply.github.com>
2018-12-11 10:53:16 +0000

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -3,17 +3,27 @@ let myButtonAdd = document.getElementById("myButtonAdd");
 let myButtonChange = document.getElementById("myButtonChange");
 let myCanvas = document.getElementById("myCanvas");
 let ctx = myCanvas.getContext("2d");
-myButtonChange.addEventListener("click", changeImage,true);
-myButtonAdd.addEventListener("click", addFilter,true);
+myButtonChange.addEventListener("click", changeImage, true);
+myButtonAdd.addEventListener("click", addFilter, true);
 
 
-myImage.onload = function() {
+myImage.onload = function () {
     myCanvas.width = myImage.width;
-   myCanvas.height = myImage.height;
+    myCanvas.height = myImage.height;
     ctx.drawImage(myImage, 0, 0, myCanvas.width, myCanvas.height);
-}; 
+};
 
 
-function changeImage(){}
+function changeImage() {
+    let myRandom = Math.floor(Math.random() * 9) + 1;
+    myImage.src = "images/" + myRandom + ".jpg";
+    myImage.onload = function () {
+        myCanvas.width = myImage.width;
+        myCanvas.height = myImage.height;
+        ctx.drawImage(myImage, 0, 0, myCanvas.width, myCanvas.height);
+    };
 
-function addFilter(){}
+}
+
+
+function addFilter() { }

c737e05be8cea8f2ba5ad8eacbdeef4ce0c05aee
JacobSwetmore <32363293+JacobSwetmore@users.noreply.github.com>
2018-12-11 10:41:23 +0000

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+let myImage = document.getElementById("myImage");
+let myButtonAdd = document.getElementById("myButtonAdd");
+let myButtonChange = document.getElementById("myButtonChange");
+let myCanvas = document.getElementById("myCanvas");
+let ctx = myCanvas.getContext("2d");
+myButtonChange.addEventListener("click", changeImage,true);
+myButtonAdd.addEventListener("click", addFilter,true);

e67f7aedcf55c9efadc11e811cd9e24552c02108
JacobSwetmore <32363293+JacobSwetmore@users.noreply.github.com>
2018-12-11 11:17:42 +0000

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -28,38 +28,37 @@ function changeImage() {
 
 function addFilter() {
 
-    // // Read each pixel out of canvas
-    // // Array representing, r then g then b values for each pixel
-    // let imageData = ctx.getImageData(0, 0, myCanvas.width, myCanvas.height);
-    // let dataArray = imageData.data;
+    // Read each pixel out of canvas
+    // Array representing, r then g then b values for each pixel
+    let imageData = ctx.getImageData(0, 0, myCanvas.width, myCanvas.height);
+    let dataArray = imageData.data;
 
 
-    // // Loop over each pixel
-    // const l = dataArray.length;
-    // for (let i = 0; i < l; i += 4) {
-    //     // dataArray [i] = Red
-    //     // dataArray [i+1] = Green
-    //     // dataArray [i+2] = Blue
-    //     // dataArray [i+3] = Alpha
+    // Loop over each pixel
+    const l = dataArray.length;
+    for (let i = 0; i < l; i += 4) {
+        // dataArray [i] = Red
+        // dataArray [i+1] = Green
+        // dataArray [i+2] = Blue
+        // dataArray [i+3] = Alpha
 
-    //     // Invert the colours by altering the r, g, b values
-    //     //     dataArray[i] = 255 - dataArray[i];
-    //     //     dataArray[i + 1] = 255 - dataArray[i + 1];
-    //     //     dataArray[i + 2] = 255 - dataArray[i + 2];
-    //     let brightness = 0.34 * dataArray[i] + 0.5 * dataArray[i + 1] + 0.16 * dataArray[i + 2];
-    //     // red
-    //     dataArray[i] = brightness;
-    //     // green
-    //     dataArray[i + 1] = brightness;
-    //     // blue
-    //     dataArray[i + 2] = brightness;
-    // }
+        // Invert the colours by altering the r, g, b values
+        //     dataArray[i] = 255 - dataArray[i];
+        //     dataArray[i + 1] = 255 - dataArray[i + 1];
+        //     dataArray[i + 2] = 255 - dataArray[i + 2];
+
+        dataArray[i] = dataArray[i];
+        // green
+        dataArray[i + 1] = 0;
+        // blue
+        dataArray[i + 2] = 0;
+    }
     // Write the data back to canvas
 
-    // ctx.putImageData(imageData, 0, 0);
+    ctx.putImageData(imageData, 0, 0);
 
-    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-    ctx.filter = "sepia(1)"; 
-    ctx.drawImage(myImage, 0, 0, myCanvas.width, myCanvas.height);
+    // ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    // ctx.filter = "sepia(1)"; 
+    // ctx.drawImage(myImage, 0, 0, myCanvas.width, myCanvas.height);
 
 }
